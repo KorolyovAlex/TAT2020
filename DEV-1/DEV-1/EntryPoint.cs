7b80ea279b7246bfa08277ba30363c4562d0e248
KorolyovAlex <korolev.prostak@gmail.com>
2020-02-26 23:56:22 +0300

    Initial commit

---
+++ DEV-1/DEV-1/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DEV_1
+{
+    /// <summary>
+    /// Entry point of the program
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Enter your line:");
+            string line = Console.ReadLine();
+            StringAnalyzer analyzer = new StringAnalyzer();
+            Console.WriteLine(analyzer.CountSequence(line));
+        }
+    }
+}
---
+++ DEV-1/DEV-1/StringAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿namespace DEV_1
+{
+    /// <summary>
+    /// Analyzer of entred line
+    /// </summary>
+    class StringAnalyzer
+    {
+        /// <summary>
+        /// Counts the length of the longest sequence of similar symbols
+        /// </summary>
+        /// <param name="str"> Line that was entered in console </param>
+        /// <returns> The length of the longest sequence of similar symbols </returns>
+        public int CountSequence(string str)
+        {
+            int maxLength = 1, counter = 1;
+            str.ToLower();
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1])
+                {
+                    counter++;
+                }
+                else
+                {
+                    if (counter > maxLength)
+                    {
+                        maxLength = counter;
+                    }
+                    counter = 1;
+                }
+            }
+            return maxLength;
+        }
+    }
+}
