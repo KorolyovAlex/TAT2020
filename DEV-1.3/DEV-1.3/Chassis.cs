1515ee03df6c3e59237d391b7fc8fd612353413b
KorolyovAlex <korolev.prostak@gmail.com>
2020-03-22 16:32:11 +0300

    Add Chassis class

---
+++ DEV-1.3/DEV-1.3/Chassis.cs
@@ -0,0 +1,16 @@
+﻿namespace DEV_1._3
+{
+    class Chassis
+    {
+        public int WheelsNumber { get; private set; }
+        public string SerialNumber { get; private set; }
+        public int MaxLoad { get; private set; }
+
+        public Chassis(int wheelsNumber, string serialNumber, int maxLoad)
+        {
+            WheelsNumber = wheelsNumber;
+            SerialNumber = serialNumber;
+            MaxLoad = MaxLoad;
+        }
+    }
+}
