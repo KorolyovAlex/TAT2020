615aa54c2fe9186f517e75721256439d892f3365
KorolyovAlex <korolev.prostak@gmail.com>
2020-03-22 16:22:25 +0300

    Add Engine class

---
+++ DEV-1.3/DEV-1.3/Engine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1._3
+{
+    class Engine
+    {
+        public int Power { get; private set; }
+        public double Capacity { get; private set; }
+        public string Type { get; private set; }
+        public string SerialNumber { get; private set; }
+
+        public Engine(int power, double capacity, string type, string serialNumber)
+        {
+            Power = power;
+            Capacity = capacity;
+            Type = type;
+            SerialNumber = serialNumber;
+        }
+    }
+}
