8a68351ad9b1d030c5850fc583889257c5599d10
KorolyovAlex <korolev.prostak@gmail.com>
2020-04-08 01:45:10 +0300

    Add car class

---
+++ DEV-2.1/DEV-2.1/Car.cs
@@ -0,0 +1,29 @@
+﻿namespace DEV_2._1
+{
+    class Car
+    {
+        private int _price;
+
+        public Car(int price, string brand, string model)
+        {
+            Price = price;
+            Brand = brand;
+            Model = model;
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if(value > 0)
+                {
+                    _price = value;
+                }
+            }
+        }
+
+        public string Brand { get; }
+        public string Model { get; }
+    }
+}
