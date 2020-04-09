148d80a439080ac9a6b793ea0dcc0bf729e7a1b3
KorolyovAlex <korolev.prostak@gmail.com>
2020-04-09 17:23:56 +0300

    Add comments for CarDealership class

--- DEV-2.1/DEV-2.1/CarDealership.cs
+++ DEV-2.1/DEV-2.1/CarDealership.cs
@@ -4,17 +4,34 @@ using System.Linq;
 
 namespace DEV_2._1
 {
+   /// <summary>
+   /// Class of car dealership
+   /// </summary>
     class CarDealership
     {
+        /// <summary>
+        /// List of cars in stock
+        /// </summary>
         public List<Car> Cars { get; }
 
+        /// <summary>
+        /// Constructor of the class
+        /// </summary>
         private CarDealership()
         {
             Cars = new List<Car>();
         }
 
+        /// <summary>
+        /// Instance of the class that implements Singleton
+        /// </summary>
         public static CarDealership Current { get; } = new CarDealership();
 
+        /// <summary>
+        /// Method that adds cars to the list
+        /// </summary>
+        /// <param name="car">Car to add</param>
+        /// <param name="amount">Number of cars</param>
         public void AddCars(Car car, uint amount)
         {
             for (int i = 0; i < amount; i++)
@@ -23,11 +40,19 @@ namespace DEV_2._1
             }
         }
 
+        /// <summary>
+        /// Method that counts the number of cars in stock
+        /// </summary>
+        /// <returns>Number of cars</returns>
         public int GetCarCount()
         {
             return Cars.Count;
         }
 
+        /// <summary>
+        /// Method that counts the average price of cars in stock
+        /// </summary>
+        /// <returns>The average price of cars</returns>
         public double GetAveragePrice()
         {
             if (Cars.Count != 0)
@@ -37,6 +62,11 @@ namespace DEV_2._1
             return 0;
         }
 
+        /// <summary>
+        /// Method that counts the average price of certain brand cars
+        /// </summary>
+        /// <param name="brand">Brand of cars to count average price</param>
+        /// <returns>The average price of certain brand cars</returns>
         public double GetAveragePriceByBrand(string brand)
         {
             if (Cars.Any(x => x.Brand.ToLower() == brand.ToLower()))
@@ -46,6 +76,10 @@ namespace DEV_2._1
             throw new ArgumentException("There's no cars with such brand");
         }
 
+        /// <summary>
+        /// Method that count the number of brands of cars in stock
+        /// </summary>
+        /// <returns>The number of brands of cars</returns>
         public int GetTypesCount()
         {
             return Cars.Select(x => x.Brand).Distinct().Count();
